f709b8f5a4416a275440ad9fa0a0b681c79f519c
kiz4ru <ladrongue5@gmail.com>
2024-09-15 19:29:32 +0200

    mejoras en movil

--- manejo.js
+++ manejo.js
@@ -2,14 +2,12 @@ function toggleMenu() {
     const menuIcon = document.querySelector('.menu-icon');
     const closeIcon = document.querySelector('.close-icon');
     const navLinks = document.getElementById('nav-links');
-    const header = document.querySelector('header');
     const body = document.body;
 
-    // Alterna clases para mostrar/ocultar elementos y gestionar la cortina
+    // Alterna la visibilidad de los iconos y del menú
     menuIcon.classList.toggle('active');
     closeIcon.classList.toggle('active');
     navLinks.classList.toggle('active');
-    header.classList.toggle('menu-open');
 
     // Desactiva el scroll cuando el menú está abierto
     if (navLinks.classList.contains('active')) {
@@ -18,3 +16,23 @@ function toggleMenu() {
         body.classList.remove('no-scroll');
     }
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuIcon = document.querySelector('.menu-icon');
+    const closeIcon = document.querySelector('.close-icon');
+
+    // Oculta la X por defecto
+    closeIcon.style.display = 'none';
+
+    menuIcon.addEventListener('click', function() {
+        closeIcon.style.display = 'block';
+        menuIcon.style.display = 'none';
+        toggleMenu();
+    });
+
+    closeIcon.addEventListener('click', function() {
+        closeIcon.style.display = 'none';
+        menuIcon.style.display = 'block';
+        toggleMenu();
+    });
+});
